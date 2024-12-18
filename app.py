058ea21e1e9736a45ff3938d597622932a22395a
qudwn1114 <qudwn1114@gmail.com>
2024-12-18 12:39:00 +0900

    fix: host

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def print_receipt():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='127.0.0.1', port=5050)
